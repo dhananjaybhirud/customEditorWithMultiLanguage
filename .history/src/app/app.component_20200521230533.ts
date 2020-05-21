5dae1fc71f91e782e4625adcd5e75e7f90097733
dhananjaybhirud <dhananjay.b@mobinexttech.com>
2020-05-21 23:15:43 +0530

    multilanguage added

---
+++ .history/src/app/app.component_20200521230533.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {_} from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
+
+import defaultLanguage from './../assets/i18n/en.json';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    curLang = 'en';
+    color;
+    cTitle;
+    cPara\;
+    cLink;
+
+    fromCode = _('demo.text-in-code');
+
+    constructor(private translate: TranslateService) {
+        translate.setTranslation('en', defaultLanguage);
+        translate.setDefaultLang('en');
+    }
+
+    useLanguage(language: string) {
+        this.translate.use(language);
+        this.curLang = language;
+    }
+}
---
+++ .history/src/app/app.module_20200521230717.ts
@@ -0,0 +1,43 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { ColorPickerModule } from 'ngx-color-picker';
+import {FormsModule} from '@angular/forms'
+import { from } from 'rxjs';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        // configure the imports
+        HttpClientModule,
+        ColorPickerModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
